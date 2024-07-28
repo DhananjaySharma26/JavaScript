6719f7e0f1a863050f63e36cb3528d6ec0ce27c3
DhananjaySharma26 <147037151+DhananjaySharma26@users.noreply.github.com>
2024-07-28 08:56:40 +0000

    01_Variable Config Done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,20 @@
+const accountId=14444
+let accountEmail="vishu.mpsharma@gmail.com"   
+var accountPassword="12345"
+accountCity="Delhi"    //By Default  
+
+let accountName        //Undefined
+
+// accountId=2.         not Allowed
+
+
+accountEmail="hc@gmail.com"
+accountPassword="234"
+accountCity="Mumbai"
+
+
+//Prefer not to use Var because of issue of block Scope
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountName])
+
