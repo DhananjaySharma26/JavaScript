ba9ff2ef2c62b71b115c14ba6b27b2a08253bdc4
DhananjaySharma26 <147037151+DhananjaySharma26@users.noreply.github.com>
2024-07-28 16:35:23 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,21 @@
+"Use Strict"      // treat all JS Code as a newer Version
+
+// alert(3+3)       //We Are using node.js, not Browser
+
+let name ='Vishu'
+let Age= 21
+
+let Me
+
+let Me1=null       
+
+// Premetive
+// Number(2 to the power 53), bigInt, boolean ,null= Stand Alone Value, undefined = Value not Assigned, Symbol = Uniqueness
+
+
+// Non= Premetive
+// object
+
+console.log(typeof(name))      //Type is String
+console.log(typeof(Me))        //Undefined
+console.log(typeof(Me1))       //The type of Null is Object
