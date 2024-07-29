e3ff2b9aee11b47b097d3ad2756af1958cce4753
DhananjaySharma26 <147037151+DhananjaySharma26@users.noreply.github.com>
2024-07-29 10:40:39 +0000

    DataTypes-Summary

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -9,13 +9,52 @@ let Me
 
 let Me1=null       
 
-// Premetive
-// Number(2 to the power 53), bigInt, boolean ,null= Stand Alone Value, undefined = Value not Assigned, Symbol = Uniqueness
+
+// Primitive
+
+/*String
+Number(2 to the power 53),
+bigInt, 
+boolean ,
+null= Stand Alone Value, 
+undefined = Value not Assigned, 
+Symbol = Uniqueness*/
 
 
-// Non= Premetive
-// object
+// Non-Premetive
+// Array, object, Function
 
 console.log(typeof(name))      //Type is String
 console.log(typeof(Me))        //Undefined is the type
 console.log(typeof(Me1))       //The type of Null is Object
+
+
+const id=Symbol('123')
+const id1=Symbol('123')
+console.log();
+console.log(id === id1);
+
+
+
+//Array
+
+let heros=["Shaktiman", "Naagraj", "Doga"]
+
+//Object
+let Obj1= {
+    name: "Vishu",
+    Age: 21,
+
+}
+
+//Function
+
+let myFunction= function(){
+    console.log("hello World")
+}
+
+console.log();
+console.log();
+console.log(typeof heros);
+console.log(typeof Obj1);
+console.log(typeof myFunction);
