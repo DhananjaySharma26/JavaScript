3ba883e86f1945134d91dfc0dd5747133d7f6e3b
DhananjaySharma26 <147037151+DhananjaySharma26@users.noreply.github.com>
2024-07-29 10:26:18 +0000

    Comparision

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,11 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);          //true
+
+console.log()
+console.log(undefined>0);      
+console.log(undefined==0);
+console.log(undefined>=0); 
+console.log()
+
+console.log("2" === 2)         //false as it check both the value and the datatype
