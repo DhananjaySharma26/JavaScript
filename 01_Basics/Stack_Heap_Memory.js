b575f8f16223bab5b948e763f8a1fcbd1d58c9ea
DhananjaySharma26 <147037151+DhananjaySharma26@users.noreply.github.com>
2024-07-29 16:30:16 +0000

    Stack And Heap Memory

---
+++ 01_Basics/Stack_Heap_Memory.js
@@ -0,0 +1,29 @@
+//Stack(Primitive DataTypes),      Heap(Non-Primitive DataTypes)
+
+
+
+
+let youtubeChannel="Code with Vishu"
+
+let anotherName= youtubeChannel
+
+anotherName="Chai aur Code"
+
+console.log(youtubeChannel);
+console.log(anotherName);
+
+
+let Obj1={
+    name:"Vishu",
+    Age:12,
+}
+
+let Obj2=Obj1
+
+Obj2.Age=21
+
+console.log(Obj1);
+console.log(Obj2);
+
+console.log(Obj1.Age);
+console.log(Obj2.Age);
