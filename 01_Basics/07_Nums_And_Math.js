56fdf5043c58d540e2a8c79111ea46442b4c2837
Dhananjay Shama <147037151+DhananjaySharma26@users.noreply.github.com>
2024-07-30 09:19:43 +0000

    Nums and Maths Done

---
+++ 01_Basics/07_Nums_And_Math.js
@@ -0,0 +1,25 @@
+let score=400;
+const balance=new Number(100);
+
+// console.log(score);
+// console.log(balance);     //Comes as a Object
+
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+let units=1000000
+
+// console.log(units.toLocaleString())
+
+
+
+//    ***************************   Maths    ****************************
+
+console.log(Math)
+console.log(Math.abs(4))
+console.log(Math.abs(-4))
+console.log(Math.round(3.6))
+console.log(Math.round(4.2))
+console.log(Math.ceil(4.1))
+console.log(Math.floor(4.4))
