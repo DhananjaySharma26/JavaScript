dd797d0cacf51973e771b6aabf25caf0d224d1d7
Dhananjay Shama <147037151+DhananjaySharma26@users.noreply.github.com>
2024-08-01 15:07:57 +0000

    Objects Part3 Done

---
+++ 02_Basics/05_Objects.js
@@ -0,0 +1,11 @@
+const course={
+    courseNmae: "JS in hindi",
+    courseFees: "999",
+    courseMentor: "Vishu"
+}
+
+// course.courseMentor
+
+const {courseMentor:Mentor}=course     //De-Structure
+
+console.log(Mentor);
