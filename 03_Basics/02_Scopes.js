d156cbd81f51bf57acb9645edd9e5f43afb5af34
Dhananjay Shama <147037151+DhananjaySharma26@users.noreply.github.com>
2024-08-02 18:29:15 +0000

    Scope Done

---
+++ 03_Basics/02_Scopes.js
@@ -0,0 +1,20 @@
+
+
+function one(){
+
+    const userName="Vishu"
+
+    function two(){
+        const website="Youtube"
+
+        console.log(userName);
+        
+    }
+
+    // console.log(website);
+
+    two()
+    
+}
+
+one()
