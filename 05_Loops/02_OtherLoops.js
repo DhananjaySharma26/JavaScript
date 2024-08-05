b1793a06df8e6b451aa47ce8a2e38a9913005cac
Dhananjay Shama <147037151+DhananjaySharma26@users.noreply.github.com>
2024-08-05 15:15:48 +0000

    Loops Done

---
+++ 05_Loops/02_OtherLoops.js
@@ -0,0 +1,22 @@
+//While
+
+
+let myarr=["Vishu", "Sharma", "Dhananjay"]
+let i=0
+while (i<myarr.length) {
+    
+    if (myarr[i]=="Dhananjay") {
+        console.log(i);
+    }
+    i++
+}
+
+
+
+
+//Do-While
+i=0
+do {
+    console.log(myarr[i]);
+    i++
+} while (i<myarr.length);
