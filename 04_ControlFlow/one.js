1f069680dce72ad32c1890b47cc1966f8ffb7506
Dhananjay Shama <147037151+DhananjaySharma26@users.noreply.github.com>
2024-08-05 09:40:09 +0000

    Control Flow Done

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,34 @@
+//if
+if(true){
+
+}
+
+
+// <, >, <=, =>, ==, !=, ===(Also check the datatype)
+
+
+// Falsy Values
+// false, undefined, null, 0, -0, "", NAN
+
+// truthy Values
+// "0", "False", " ", [], {}, fucntion(){}, 
+
+
+
+//Nullish Coalescing Operator(??):  for  null and undefined
+let val1
+val1= 5 ?? 10         //Takes the first Value and if the first value id null or undefined then it takes the third value
+
+console.log(val1);
+
+
+
+let val2
+val2= undefined ?? 10
+
+console.log(val2);
+
+
+
+//Terniary Operator
+//  condition? True :False
