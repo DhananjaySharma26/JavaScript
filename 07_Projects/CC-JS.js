23567b600bb5c9496de8f9e8396a6066babcf75b
Dhananjay Shama <147037151+DhananjaySharma26@users.noreply.github.com>
2024-08-10 09:12:37 +0000

    Project 1 Done

---
+++ 07_Projects/CC-JS.js
@@ -0,0 +1,23 @@
+const buttons=document.querySelectorAll('.button')
+
+const body=document.querySelector("body")
+
+buttons.forEach((button)=>{
+
+    //event listner
+    button.addEventListener('click',(e)=>{
+        console.log(e);
+        console.log(e.target);
+
+        if(e.target.id=='grey'){
+            body.style.backgroundColor= e.target.id
+        }else if(e.target.id=='white'){
+            body.style.backgroundColor= e.target.id
+        }else if(e.target.id=='blue'){
+            body.style.backgroundColor= e.target.id
+        }else{
+            body.style.backgroundColor= e.target.id
+        }
+    })
+    
+})
